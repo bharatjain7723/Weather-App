138fdea8f77d2839f4682ef1183602e72b0d544b
Bharat Jain <bharatjain7723@gmail.com>
2018-07-21 09:20:40 +0530

    init

---
+++ playground/async-basics.js
@@ -0,0 +1,13 @@
+console.log("Starting App");
+
+setTimeout(() =>{
+ console.log("inside");
+}, 2000);
+
+setTimeout(() =>{
+    console.log("second timeout ");
+    
+}, 0);
+
+console.log("Finishing App");
+
---
+++ playground/callback.js
@@ -0,0 +1,4 @@
+var getUser = (id, callback) => {
+
+};
+
